d528ad065a1d247747a4238c354f9c98f32009ff
Robert <robert913152750@gmail.com>
2020-09-25 16:20:46 +0800

    edit basic getRestaurants

--- services/restService.js
+++ services/restService.js
@@ -1,12 +1,23 @@
-// const db = require('../models')
+const db = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
+const City = db.City
 
 const restService = {
   getRestaurants: (req, res, callback) => {
-    callback({
-      '1': 'Hello',
-      '2': 'world'
+    return Restaurant.findAll({
+      include: [
+        { model: Category },
+        { model: City }
+      ]
+    }).then(restaurants => {
+      callback({ restaurants: restaurants })
     })
   }
+
+
+
+
 }
 
 module.exports = restService
