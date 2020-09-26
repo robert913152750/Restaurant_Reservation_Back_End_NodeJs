48e01f1a7bef304cbf31b5b2dc46466a4561a97c
Robert <robert913152750@gmail.com>
2020-09-26 19:01:59 +0800

    authencated middleware

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+const passport = require('./config/passport')
 
 app.use(bodyParser.urlencoded({ extended: true }))
+
 app.listen(process.env.PORT, () => {
   console.log('app is running on express ')
 })
--- routes/apis.js
+++ routes/apis.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../config/passport')
 const restController = require('../controllers/api/restController')
 const userController = require('../controllers/api/userController')
 
-//JWT signin
+//passport middleware
+const authenticated = passport.authenticate('jwt', { session: false })
+
+const authenticatedBusiness = (req, res, next) => {
+  if (req.user) {
+    if (req.user.role === 'business') return next()
+    return res.json({ status: 'error', message: 'permission denied' })
+  } else {
+    return res.json({ status: 'error', message: 'permission denied' })
+  }
+}
+
+//router
 router.post('/signin', userController.signIn)
-// restaurant route
-router.get('/home', restController.getRestaurants)
-router.get('/restaurant/:id', restController.getRestaurant)
+router.get('/home', authenticated, restController.getRestaurants)
+router.get('/restaurant/:id', authenticated, restController.getRestaurant)
 
 module.exports = router
