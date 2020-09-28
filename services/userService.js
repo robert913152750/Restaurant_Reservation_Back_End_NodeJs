1d9de9bfe4295ce0297a61b65870e0af33437e76
Robert <robert913152750@gmail.com>
2020-09-28 23:14:03 +0800

    create post comment

--- services/userService.js
+++ services/userService.js
@@ -4,6 +4,17 @@ const Comment = db.Comment
 
 
 let userService = {
+  postComment: (req, res, callback) => {
+    Comment.create({
+      UserId: req.user.dataValues.id,
+      RestaurantId: req.body.RestaurantId,
+      content: req.body.content,
+      rating: req.body.rating
+    }).then(comment => {
+      return callback({ comment: comment })
+    })
+      .catch(err => res.send(err))
+  }
 
 }
 
