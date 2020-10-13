75883b6d95ec9c3c4b061309682f491de7e7d04e
Robert <robert913152750@gmail.com>
2020-10-13 18:24:59 +0800

    basic getUserInfo feature

--- routes/apis.js
+++ routes/apis.js
@@ -21,17 +21,22 @@ const authenticatedBusiness = (req, res, next) => {
 }
 
 //router
+//basic
 router.post('/signup', userController.signUp)
 router.post('/signin', userController.signIn)
 router.get('/get_current_user', authenticated, userController.getUser)
 router.get('/home', restController.getRestaurants)
 router.get('/restaurant/:id', restController.getRestaurant)
 router.get('/reservation/:id', restController.getMeals)
+
+//common_user
 router.post('/comment', authenticated, userController.postComment)
 router.post('/reservation/:id', authenticated, userController.postReservation)
 router.get('/member/:id/orders', authenticated, userController.getOrders)
 router.put('/member/edit', authenticated, upload.single('image'), userController.putUser)
+router.get('/member/info', authenticated, userController.getUserInfo)
 
+//business_user
 router.get('/business/restaurant', authenticated, authenticatedBusiness, businessController.getRestaurant)
 router.get('/business/:id/menu', authenticated, authenticatedBusiness, businessController.getMenu)
 router.put('/business/:id/restaurant', authenticated, authenticatedBusiness, upload.single('image'), businessService.putRestaurant)
