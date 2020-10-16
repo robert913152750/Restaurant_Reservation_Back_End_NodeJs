c5df2c868f8b6c726cb8fd5a2067ab43041897f3
Robert <robert913152750@gmail.com>
2020-10-16 12:19:22 +0800

    fix business_putMeal if isSale is null setting(fix little bug)

--- services/businessService.js
+++ services/businessService.js
@@ -111,7 +111,7 @@ const businessService = {
   async putMenu (req, res, callback) {
     try {
       const { MealId, name, MealCategoryId, description, price, isSale } = req.body
-      if (isSale === 'null') return isSale = false
+      if (isSale === null) return isSale = false
       if (!name || !MealCategoryId || !price) {
         return callback({ status: 'error', message: '名稱、類別、價格為必填' })
       }
